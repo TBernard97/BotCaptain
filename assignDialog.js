e350a459772f2a5f6535a826e8e3e2c12e35bd39
TBernard97 <darby.bernard@gmail.com>
2021-11-12 14:31:47 -0500

    Refactor/cleanup for assignDialog.js

--- assignDialog.js
+++ assignDialog.js
@@ -2,13 +2,14 @@ const { ComponentDialog, TextPrompt, WaterfallDialog, DialogTurnStatus} = requir
 const { CardFactory } = require('botbuilder');
 const taskId = 'taskDialog';
 const jsonfile = require('jsonfile');
-const xAPI = require('./xAPI_Statements.js');
 //Prompt for Task
 const { TaskPrompt } = require('./prompts/taskPrompt');
 const GET_TASK_PROMPT = 'taskPrompt';
 //Import a card template
 const TaskCard = require('./cards/TaskCard.json');
 const { fileIO } = require('./fileIO.js');
+const { xAPI_Statements } = require('./xAPI_Statements.js');
+const xAPI_Handler = new xAPI_Statements();
 
 class AssignDialog extends ComponentDialog {
     
@@ -36,19 +37,27 @@ class AssignDialog extends ComponentDialog {
     async promptForTask(step){
         step.values.task = {};
         
-        //Need to read profiles to get proper vote count
+        //Need to read profiles to get proper vote count. Profile passed to function may not have proper
         step.values.profileTable = jsonfile.readFileSync(`Resources/Classes/${step.options.profile.class}/profiles.json`);
         step.values.profile = step.values.profileTable[`${step.options.profile.name}`];
 
-        var tasks = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/tasks.json`);
-        let task_list = Object.keys(tasks);
-        return await step.prompt(GET_TASK_PROMPT, 'What task would you like to assign?',task_list);
+        //Used for indexing based on user count
+        step.values.profileNamesList = Object.keys(step.values.profileTable);
+        
+        step.values.tasksTable = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/tasks.json`);
+        step.values.tasksList = Object.keys(step.values.tasksTable);
+
+        step.values.voteTable = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/votes.json`);
+
+        
+
+        return await step.prompt(GET_TASK_PROMPT, 'What task would you like to assign?',step.values.tasksList);
     }
 
     async captureTask(step){
-        var tasks = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/tasks.json`);
+       
         step.values.task_id = step.result.value;
-        step.values.task = tasks[`${step.result.value}`];
+        step.values.task = step.values.tasksTable[`${step.result.value}`];
 
         //If student already voted for a leader, don't even bother with remaining portions of dialog
         if(step.values.profile.votes[`${step.values.task_id}`] === true){
@@ -61,26 +70,23 @@ class AssignDialog extends ComponentDialog {
     }
 
     async buildCard(step){
-        var task = step.values.task;
-        var task_id = step.values.task_id;
-        var profiles = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/profiles.json`);
-        TaskCard["body"][0].id = task_id;
-        TaskCard["body"][3].text = task.description;
-        let profiles_list = Object.keys(profiles);
+
+        TaskCard["body"][0].id = step.values.task_id;
+        TaskCard["body"][3].text = step.values.task.description;
         
         
-        for (var user in profiles_list){
-            var choices = TaskCard["body"][5].choices
-            let equal = step.values.profile.team == profiles[profiles_list[user]].team;
+        for (var user in step.values.profileNamesList){
+            var choices = TaskCard["body"][5].choices;
+            let equal = step.values.profile.team == step.values.profileTable[step.values.profileNamesList[user]].team;
             if(equal == true){
                 if(choices[user] != undefined){
                     choices[user] = {"title": "", "value": ""};
-                    choices[user].title = profiles[profiles_list[user]].nick;  
-                    choices[user].value = profiles[profiles_list[user]].nick;
+                    choices[user].title = step.values.profileTable[step.values.profileNamesList[user]].nick;  
+                    choices[user].value = step.values.profileTable[step.values.profileNamesList[user]].nick;
                 } else {
                     var choiceObject = {"title": "", "value": ""};
-                    choiceObject.title = profiles[profiles_list[user]].nick; 
-                    choiceObject.value = profiles[profiles_list[user]].nick; 
+                    choiceObject.title = step.values.profileTable[step.values.profileNamesList[user]].nick; 
+                    choiceObject.value = step.values.profileTable[step.values.profileNamesList[user]].nick; 
                     choices.push(choiceObject);
                 }
                 
@@ -102,19 +108,17 @@ class AssignDialog extends ComponentDialog {
     }
 
     async captureStudent(step){
-        var profiles = jsonfile.readFileSync(`Resources/Classes/${step.values.profile.class}/profiles.json`);
-        let profiles_list = Object.keys(profiles);
         
         var user_nicks = [];
-        for (var user in profiles_list){
-            let equal = step.values.profile.team == profiles[profiles_list[user]].team;
+        for (var user in step.values.profileNamesList){
+            let equal = step.values.profile.team == step.values.profileTable[step.values.profileNamesList[user]].team;
             if(equal == true){
-                user_nicks.push(profiles[profiles_list[user]].nick);
+                user_nicks.push(step.values.profileTable[step.values.profileNamesList[user]].nick);
             }
       
         }
 
-        //If use enters text instead of using card, ensure user is valid
+        //If user enters text instead of using card, ensure user is valid (i.e. User is present in the )
         if(step.context.activity.value != undefined){
             step.values.vote = step.context.activity.value;
             return await step.next();
@@ -122,7 +126,8 @@ class AssignDialog extends ComponentDialog {
             step.values.vote = {"leaderSelection": `${step.context.activity.text}`};
             return await step.next();
         } else {
-            await step.context.sendActivity("Need a valid user.");
+            await step.context.sendActivity("Need a valid user. Ending dialog.");
+        
             return await step.endDialog();
         }
     
@@ -130,42 +135,39 @@ class AssignDialog extends ComponentDialog {
 
     async recordVote(step){
 
-        var votePath = `Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/votes.json`;
-        var votes = jsonfile.readFileSync(votePath);
-        let student = step.values.vote.leaderSelection;
-        let leaderVoteObject = {votes:0};
-        let voteTaskId = votes[`${step.values.task_id}`];
-        let xAPI_Handler = new xAPI.xAPI_Statements;
+        const votePath = `Resources/Classes/${step.values.profile.class}/Teams/${step.values.profile.team}/votes.json`;
+
+        var leaderVoteObject = {votes:0};
         
+        // If vote table has student entry, simply add one.  
+        // Else need to compensate for student not present or task table entry itself not being present
+        if(step.values.voteTable[`${step.values.task_id}`] != undefined && step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`] != undefined){
+            step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`].votes = step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`].votes + 1; 
+            jsonfile.writeFileSync(votePath, step.values.voteTable, {flags:'w'});
 
-        if(voteTaskId != undefined && voteTaskId[`${student}`] != undefined){
-            voteTaskId[`${student}`].votes = voteTaskId[`${student}`].votes + 1; 
-            jsonfile.writeFileSync(votePath, votes, {flags:'w'});
-
-        } else if(voteTaskId != undefined && voteTaskId[`${student}`] === undefined) {
-            
+        } else if(step.values.voteTable[`${step.values.task_id}`] != undefined && step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`] === undefined) {
             leaderVoteObject.votes = 1;
-            voteTaskId[`${student}`] = leaderVoteObject;
-            jsonfile.writeFileSync(votePath, votes, {flags:'w'});
+            step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`] = leaderVoteObject;
+            jsonfile.writeFileSync(votePath, step.values.voteTable, {flags:'w'});
 
         } else {
 
-            votes[`${step.values.task_id}`] = {};
+            step.values.voteTable[`${step.values.task_id}`] = {};
             leaderVoteObject.votes = 1;
-            votes[`${step.values.task_id}`][`${student}`] = leaderVoteObject;
-            jsonfile.writeFileSync(votePath, votes, {flags:'w'});
+            step.values.voteTable[`${step.values.task_id}`][`${step.values.vote.leaderSelection}`] = leaderVoteObject;
+            jsonfile.writeFileSync(votePath, step.values.voteTable, {flags:'w'});
 
         }
         
-        //Remove current user from choices when completed (workaround due to state being held on card import)
+        // Remove current user from choices when completed (workaround due to state being held on card import)
         for(var choice in TaskCard["body"][5].choices){
             TaskCard["body"][5].choices.pop(choice);
         }
 
-        xAPI_Handler.recordRoleAssignment(step.options.profile.email, step.options.profile.nick, student, step.values.task_id);
+        xAPI_Handler.recordRoleAssignment(step.options.profile.email, step.options.profile.nick, step.values.vote.leaderSelection, step.values.task_id);
         step.values.profile.votes[`${step.values.task_id}`] = true;
         fileIO.insertProfile(step.values.profile);
-        await step.context.sendActivity(`Voted for ${student} to lead ${step.values.task_id}`);
+        await step.context.sendActivity(`Voted for ${step.values.vote.leaderSelection} to lead ${step.values.task_id}`);
         
         return await step.endDialog();
     }
