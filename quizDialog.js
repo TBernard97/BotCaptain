eb418c0994441fd99ce40f3a319d32cdf647f039
kryshr <krysh1998@gmail.com>
2021-11-17 15:05:00 -0500

    Added quizDialog, added empty quiz record to profileDialog

--- quizDialog.js
+++ quizDialog.js
@@ -2,8 +2,13 @@ const { ComponentDialog, TextPrompt, WaterfallDialog} = require('botbuilder-dial
 const quizId = 'quizDialog';
 const fs = require('fs');
 const jsonfile = require('jsonfile');
+const { fileIO } = require('./fileIO');
+//Module for sending out emails
+const {scheduler} = require('./scheduler.js');
+
+
+
 //Module for starting a quiz
-//const {quiz} = require('./quiz.js');
 //Prompt for Date
 const { QuizPrompt } = require('./prompts/quizPrompt');
 const GET_QUIZ_PROMPT = 'quizPrompt';
@@ -25,7 +30,7 @@ class QuizDialog extends ComponentDialog {
             this.captureQuiz.bind(this),
             this.promptForQuestion.bind(this),
             this.completeQuiz.bind(this),
-            //this.completeReminder.bind(this)
+           
          ]));
 
         //Create xAPI_Handler Object
@@ -39,8 +44,24 @@ class QuizDialog extends ComponentDialog {
 
     async promptForQuiz(step){
         step.values.quiz = {};
+        step.values.profileTable= jsonfile.readFileSync(`./Resources/Classes/${step.options.profile.class}/profiles.json`);
+        //get current date
+        var today = new Date();
+        let date = ("0" + today.getDate()).slice(-2);
+        let month = ("0" + (today.getMonth() + 1)).slice(-2);
+        let year = today.getFullYear();
+        step.values.current_date = year + '-' +  month + '-' + date
+
+        //get current time
+        var hours = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
+        var minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        var seconds = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+        step.values.time = hours + ":" + (minutes+1) + ":" + seconds;
+        console.log(step.values.time);
+        
         step.values.profile = {};
-        step.values.profile = step.options.profile;
+        step.values.profile = step.values.profileTable[`${step.options.profile.name}`];
+        console.log(step.values.profile);
         var quizzes = jsonfile.readFileSync(`./Resources/Classes/${step.values.profile.class}/quizzes.json`);
         let quiz_list = Object.keys(quizzes);
         return await step.prompt(GET_QUIZ_PROMPT, 'What Quiz would you like to see?',quiz_list);
@@ -48,14 +69,19 @@ class QuizDialog extends ComponentDialog {
 
     async captureQuiz(step){
         var quizzes = jsonfile.readFileSync(`./Resources/Classes/${step.values.profile.class}/quizzes.json`);
+        step.values.quizName= step.result.value;
         step.values.quiz = quizzes[`${step.result.value}`]
+        console.log(step.values.quiz)
+        if(step.values.profile.quizzes[`${step.values.quizName}`] == true){
+            await step.context.sendActivity("You already completed the quiz");
+            return await step.endDialog(); 
+        } else{
+            return await step.next();
         return await step.next();
-    }
+    } }
 
     async promptForQuestion(step){
-     //   var quizzes = jsonfile.readFileSync(`./Resources/Classes/${step.values.profile.class}/quizzes.json`);
-     //  let quiz_list = Object.keys(quizzes['step.values.quiz']);
-     //   step.values.question = step.value.quiz.
+
      await step.context.sendActivity(`Please choose the appropriate answer ${step.values.quiz.question}` )
      return await step.prompt(GET_QUIZ_PROMPT, 'Please choose your answer from the options', step.values.quiz.answers );
     }
@@ -68,28 +94,27 @@ class QuizDialog extends ComponentDialog {
         await step.context.sendActivity("correct")
         this.xAPI_Handler.recordQuiz(step.values.profile.email);
         await step.context.sendActivity(`Quiz result will be sent to:  ${step.values.profile.email}`);
+        step.values.message=`${step.values.quizName} Quiz result: Congrats! you studied!`;
         }
         
-        else
-        await step.context.sendActivity(`wrong, correct answer = ${step.values.quiz.correctAnswer}`  );
-        //step.values.time = {}
+        else{
+        await step.context.sendActivity(`wrong response, correct answer = ${step.values.quiz.correctAnswer}`  );
+        step.values.message=`${step.values.quizName} Quiz result: I am disappointed in you. Study harder`;
+        }
+        console.log(step.values.quiz);
+
+        step.values.profile.quizzes[`${step.values.quizName}`] = true;
+        fileIO.insertProfile(step.values.profile);
+
+        scheduler.email(`${step.values.profile.email}`,`${step.values.current_date}`, `${step.values.time}`, `${step.values.message}`);
+        
+        console.log(step.values.profile.quizzes);
         return await step.endDialog();
 
 
     }
 
- /*   async completeQuiz(step){
-        //step.values.time = step.result;
-        //step.values.schedule = {};
-        //step.values.schedule.date = step.values.date[0].value;
-        //step.values.schedule.task = step.values.task.description;
-        //scheduler.email(`${step.values.profile.email}`,`${step.values.schedule.date}`, `${step.values.time}`, `${step.values.schedule.task}`);
-        //this.xAPI_Handler.recordSchedule(step.values.profile.email);
-        await step.context.sendActivity(`Reminder will be sent to ${step.values.profile.email}, on ${step.values.schedule.date}, at ${step.values.time}.`)
-        return await step.endDialog(step.values.schedule);
-    }
-
-*/
+ 
 
 }
 
